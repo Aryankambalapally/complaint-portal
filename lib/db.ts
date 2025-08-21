4e7607ffa36a3d42f95e5e1359c4e1d52fb71fa9
Aryankambalapally <aryankambalapally7@gmail.com>
2025-08-21 23:33:21 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   // user: "postgres",
   // host: "localhost", // or your docker network IP
   // database: "postgres", // or your actual DB name
